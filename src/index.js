4d8cc5459efeb2529bef8d5b1d528f0187e6f650
gsrapha <rapha.gscorp@gmail.com>
2024-12-14 16:07:31 -0300

    implementação da função de depósito

--- src/index.js
+++ src/index.js
@@ -7,13 +7,24 @@ app.use(express.json());
 
 const customers = [];
 
-/**
- * cpf -  string
- * name - string
- * id - identtificador unico - uuid
- * statement []
- */
+// Middleware
 
+function verifyIfExistsAccountCPF(request, response, next) {
+    const { cpf } =  request.headers;
+
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    if(!customer) {
+        return response.status(400).json({ error: "Customer not found" });
+    }
+
+    request.customer = customer;
+
+    return next();
+
+}
+
+ // criação de conta - verificar se cpf ja existe
 app.post("/account", (request, response) => {
     const {cpf, name} = request.body;
 
@@ -34,7 +45,33 @@ app.post("/account", (request, response) => {
 
     return response.status(201).send();
 
+});
+
+// criação de conta - verificar se cpf ja existe
+
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+
+    const { customer } = request;
+    return response.json(customer.statement);
+
 })
+
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+    const { description, amount } = request.body;
+
+    const { customer } = request;
+
+    const statementOperation = {
+        description,
+        amount, 
+        created_at: new Date(),
+        type: "credit"
+    }
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+});
 
 app.listen(3333);
 
